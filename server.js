732ecc74ae740bfd4304606e58b298b159950b45
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-19 14:48:35 -0500

    Finished Day 2 Web API lecture walktrhough

--- server.js
+++ server.js
@@ -1,17 +1,65 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan')
+
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+function teamNamer(req, res, next) {
+  req.team = 'Web 16';
+
+  next();
+}
+
+// global middleware
 server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(teamNamer);
+// server.use(moodyGateKeeper);
 
-server.use('/api/hubs', hubsRouter)
+server.use('/api/hubs', restricted('Joe'), hubsRouter)
 
-server.get('/', (req, res) => {
-  res.send(`
-    <h2>Lambda Hubs API</h2>
-    <p>Welcome to the Lambda Hubs API</p>
-  `);
+server.get('/', restricted('po'), async (req, res, next) => {
+  if (req.headers.name === 'po') {
+    res.send(`
+      <h2>Lambda Hubs API</h2>
+      <p>Welcome ${req.team} to the Lambda Hubs API</p>
+    `);
+  } else {
+    next('any error argument will trigger');
+  }
 });
+
+server.use(errorHandler);
+
+function restricted(name) {
+  return function (req, res, next) {
+    const personName = req.headers.name;
+
+    if (personName === name) {
+      next();
+    } else {
+      res
+        .status(401)
+        .json({ messgae: 'please login to access this information' });
+    }
+  };
+};
+
+function moodyGateKeeper(req, res, next) {
+  const seconds = new Date().getSeconds();
+
+  if (seconds % 3 === 0) {
+    res.status(403).json({ you: 'shall not pass!' });
+  } else {
+    next();
+  }
+};
+
+function errorHandler(err, req, res, next) {
+  res.status(400).json({ messgae: 'Bad Panda!', err });
+}
 
 module.exports = server;
